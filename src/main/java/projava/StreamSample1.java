14d5cdb0ff3774c92416ff28db4781be651c9848
takattty <r9r_celtics_t9t@icloud.com>
2022-04-25 22:42:44 +0900

    streamについての学習

--- src/main/java/projava/StreamSample1.java
+++ src/main/java/projava/StreamSample1.java
@@ -3,17 +3,13 @@ package projava;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamSample1 {
     public static void main(String[] args) {
         var data = List.of("yamamoto", "kis", "sugiyama");
 
-        var result = new ArrayList<String>();
-        for(var s : data) {
-            if (s.length() >= 5) {
-                result.add(s);
-            }
-        }
+        var result = data.stream().filter(s -> s.length() >= 5).collect(Collectors.toCollection(ArrayList::new));
         System.out.println(result);
     }
 }
--- src/main/java/projava/StreamSample2.java
+++ src/main/java/projava/StreamSample2.java
@@ -1,18 +1,20 @@
 package projava;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class StreamSample2 {
     public static void main(String[] args) {
         var data = List.of("yamamoto", "kis", "sugiyama");
 
-        var result = 0;
-        for(var s : data) {
-            if (s.length() >= 5) {
-                result++;
-            }
-        }
+        var result = (int) data.stream().filter(s -> s.length() >= 5).count();
         System.out.println(result);
+
+        var nums = new int[]{2, 3, 5};
+        System.out.println(IntStream.of(nums).sum());
+        System.out.println(IntStream.range(0, 10).toArray());
+        System.out.println(IntStream.rangeClosed(0, 10).toArray());
+
     }
 }
 
