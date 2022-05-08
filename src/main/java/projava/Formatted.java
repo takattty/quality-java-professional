6f8c87ad26432692db70a18f813e45672bfe9b61
takattty <r9r_celtics_t9t@icloud.com>
2022-05-08 22:49:13 +0900

    modify Formatted class

--- src/main/java/projava/Formatted.java
+++ src/main/java/projava/Formatted.java
@@ -5,17 +5,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class Formatted {
-    public static String formmat(String number) {
-        String result = "";
+    String text;
+
+    Formatted(String text) {
+        this.text = text;
+    }
+    public String format(int number) {
+        System.out.println(this.text);
+        String[] textArray = this.text.split("%");
+        for(String element : textArray) {
+            System.out.println(element);
+        }
+
+        String strNumber = String.valueOf(number);
+        String result;
         List<String> list = new ArrayList<>();
         int index = 1;
 
-        for (int i = number.length()-1; i >= 0; i--) {
+        for (int i = strNumber.length()-1; i >= 0; i--) {
             if (index == 4) {
                 list.add(",");
                 index = 1;
             }
-            list.add(String.valueOf(number.charAt(i)));
+            list.add(String.valueOf(strNumber.charAt(i)));
             index++;
         }
 
--- src/main/java/projava/FormattedUse.java
+++ src/main/java/projava/FormattedUse.java
@@ -2,19 +2,25 @@ package projava;
 
 public class FormattedUse {
     public static void main(String[] args) {
-        String result1 = Formatted.formmat(String.valueOf(120));
-        System.out.printf("入力：　%d", 120);
-        System.out.println();
-        System.out.println(result1);
+//        String result1 = Formatted.format(120);
+//        System.out.printf("入力：　%d", 120);
+//        System.out.println();
+//        System.out.println(result1);
+//
+//        String result2 = Formatted.format(120000000);
+//        System.out.printf("入力：　%d", 120000000);
+//        System.out.println();
+//        System.out.println(result2);
+//
+//        String result3 = Formatted.format(1200000000);
+//        System.out.printf("入力：　%d", 1200000000);
+//        System.out.println();
+//        System.out.println(result3);
 
-        String result2 = Formatted.formmat(String.valueOf(120000000));
-        System.out.printf("入力：　%d", 120000000);
-        System.out.println();
-        System.out.println(result2);
-
-        String result3 = Formatted.formmat(String.valueOf(1200000000));
+        Formatted instance = new Formatted("test%s");
+        String result4 = instance.format(1200000000);
         System.out.printf("入力：　%d", 1200000000);
         System.out.println();
-        System.out.println(result3);
+        System.out.println(result4);
     }
 }
