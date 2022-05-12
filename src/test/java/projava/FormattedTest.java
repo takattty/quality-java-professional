32154384af8efd4ff0fd43d8b399ba2d8bc7d928
takattty <r9r_celtics_t9t@icloud.com>
2022-05-12 20:56:34 +0900

    added 四桁以上の数値のテスト

--- src/test/java/projava/FormattedTest.java
+++ src/test/java/projava/FormattedTest.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("ALL")
 class FormattedTest {
 
     @Test
-    void format() {
+    void 数値のformat処理 () {
         Formatted instance = new Formatted("test%s");
         String result = instance.format(120);
         assertEquals("120", result);
+    }
+
+    @Test
+    void 四桁以上の数値のformat処理() {
+        Formatted instance  = new Formatted("test%s");
+        String result = instance.format(1200);
+        assertEquals("1,200", result);
     }
 }
