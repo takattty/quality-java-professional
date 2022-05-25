3c668e2ab117327d9553e5532c13ac68c3c0afda
takattty <r9r_celtics_t9t@icloud.com>
2022-05-25 21:04:51 +0900

    本の回答を見て修正verも追記

--- src/main/java/projava/RemoveDuplicate.java
+++ src/main/java/projava/RemoveDuplicate.java
@@ -5,16 +5,32 @@ public class RemoveDuplicate {
         // 下の文字列の重複を無くし、その結果を返す
         // 結果: abcbabc;
         String data = "abcccbaabcc";
-
         StringBuilder sb = new StringBuilder();
-        sb.append(data.charAt(0));
-        for (int i = 1; i < data.length(); i++) {
+
+//        sb.append(data.charAt(0));
+//        for (int i = 1; i < data.length(); i++) {
+//            char ch = data.charAt(i);
+//            if(ch != data.charAt(i - 1)) {
+//                sb.append(data.charAt(i));
+//            }
+//        }
+//        String strSb = sb.toString();
+//        System.out.println(data);
+//        System.out.println(sb);
+
+        // 上の処理だとsb.appendが2回出てて綺麗じゃないな。
+        // まとめたい。
+
+        for (int i = 0; i < data.length(); i++) {
             char ch = data.charAt(i);
-            if(ch != data.charAt(i - 1)) {
-                sb.append(data.charAt(i));
+            if (i > 0 && ch == data.charAt(i-1)) {
+                continue;
             }
+            sb.append(ch);
         }
-        System.out.println(sb);
+        String strSb = sb.toString();
+        System.out.println(data);
+        System.out.println(strSb);
 
     }
 }
