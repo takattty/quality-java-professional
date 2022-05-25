923ebb9d22fd753db5e278801e429e4be4afe846
takattty <r9r_celtics_t9t@icloud.com>
2022-05-25 20:22:38 +0900

    重複を無くす処理を作成

--- src/main/java/projava/RemoveDuplicate.java
+++ src/main/java/projava/RemoveDuplicate.java
@@ -2,19 +2,19 @@ package projava;
 
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        String date = "abcccbaabcc";
+        // 下の文字列の重複を無くし、その結果を返す
+        // 結果: abcbabc;
+        String data = "abcccbaabcc";
 
-        StringBuilder builder = new StringBuilder();
-        char prev = 0;
-        for(char ch : date.toCharArray()) {
-            if(ch == prev) {
-                continue;
+        StringBuilder sb = new StringBuilder();
+        sb.append(data.charAt(0));
+        for (int i = 1; i < data.length(); i++) {
+            char ch = data.charAt(i);
+            if(ch != data.charAt(i - 1)) {
+                sb.append(data.charAt(i));
             }
-            builder.append(ch);
-            prev = ch;
         }
-        String result = builder.toString();
-        System.out.println(date);
-        System.out.println(result);
+        System.out.println(sb);
+
     }
 }
